d5342153876212ca598513bbc47c47863ba79e4f
Theodore Nastos <theo@moosend.com>
2018-05-07 15:33:54 +0300

    Added cookie getter / setter for mouseOutFlag

--- src/cookies/CookieNames.ts
+++ src/cookies/CookieNames.ts
@@ -2,15 +2,18 @@ export default class CookieNames implements ICookieNames {
     public userIdName: string;
     public sessionIdName: string;
     public emailName: string;
+    public mouseOutFlagName: string;
 
     constructor(
         userIdName: string = "uid",
         sessionIdName: string = "sessionid",
         emailName: string = "email",
+        mouseOutFlagName: string = "mouseOutFlag"
     ) {
         this.userIdName = userIdName;
         this.sessionIdName = sessionIdName;
         this.emailName = emailName;
+        this.mouseOutFlagName = mouseOutFlagName;
     }
 
     public getUserIdName(): string {
@@ -35,5 +38,13 @@ export default class CookieNames implements ICookieNames {
 
     public setEmailName(emailName: string): void {
         this.emailName = emailName;
+    }
+
+    public getMouseOutFlag(): string {
+        return this.mouseOutFlagName;
+    }
+
+    public setMouseOutFlag(mouseOutFlagName: string): void {
+        this.mouseOutFlagName = mouseOutFlagName;
     }
 }
