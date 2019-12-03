e801c875ceaa4408587c0d200363a4d614b8f6cc
donibehrami <doni@moosend.com>
2019-12-03 15:30:21 +0100

    Add listeners to close icon and text if available.

--- src/subscription-forms/Popup.ts
+++ src/subscription-forms/Popup.ts
@@ -58,8 +58,9 @@ export default class Popup extends Form {
         this.setIntervalToShowCookie(formElementId, parseInt(this.settings.Exit_Show_After), this.settings.Exit_Show_Type);
 
         this.attachStyle(formEl);
-        this.attachCloseButton(formEl);
+        this.addListenerToButton(formEl);
         this.addListenerToOverlay(formEl);
+        this.addListenerToText(formEl);
 
         this.attachScripts(formEl);
     }
@@ -73,26 +74,30 @@ export default class Popup extends Form {
         formEl.insertBefore(styleGlobal, elementWrapper);
     }
 
-    attachCloseButton(formEl: HTMLElement): void {
+    addListenerToButton(formEl: HTMLElement): void {
 
-        let closeButton = document.createElement("div");
-        closeButton.className = "close-moo";
-        closeButton.innerHTML = "EXIT";
+        const icon = formEl.querySelector(`.main-form-wrapper .content .moosend-form-close-icon`);
 
-        let elementWrapper = document.querySelector(`#mooform${this.entityId} .main-form-wrapper`);
-        formEl.insertBefore(closeButton, elementWrapper);
+        icon && icon.addEventListener('click', function () {
+            formEl.remove();
+        });
+    }
 
-        closeButton.addEventListener('click', function () {
-            this.parentElement.remove();
+    addListenerToText(formEl: HTMLDivElement): void {
+
+        const text = formEl.querySelector('.main-form-wrapper .content form .moosend-form-close-text');
+
+        text && text.addEventListener('click', function () {
+            formEl.remove();
         });
     }
 
     addListenerToOverlay(formEl: HTMLElement): void {
 
-        let overlay = document.querySelector(`#mooform${this.entityId} .main-form-wrapper .moosend-popup-form-overlay`);
+        const overlay = formEl.querySelector(`.main-form-wrapper .moosend-form-close-overlay`);
 
         overlay && overlay.addEventListener('click', function () {
-            this.parentElement.parentElement.remove();
+            formEl.remove();
         });
     }
 
